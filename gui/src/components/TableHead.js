4db7cbc6c1d4b76c2a1febde5a1f21f581551fe6
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2021-01-21 19:27:25 +0100

    refactoring of tableHead

--- gui/src/components/TableHead.js
+++ gui/src/components/TableHead.js
@@ -1,25 +1,34 @@
 import TableHeader from "./TableHeader";
 
 const TableHead = (props) => {
-    const headers = [
-        "ReqIF.LongName",
-        "ReqIF.ChapterName",
-        "ReqIF.Description",
-        "ReqIF.ForeignID",
-        "ReqIF.Identifier",
-        "IE PUID",
-        "ReqIF.Name",
-        "ReqIF.Prefix",
-        "ReqIF.Text"
-    ];
+    const bgHover = "bg-primary",
+        headers = [
+            "ReqIF.LongName",
+            "ReqIF.ChapterName",
+            "ReqIF.Description",
+            "ReqIF.ForeignID",
+            "ReqIF.Identifier",
+            "IE PUID",
+            "ReqIF.Name",
+            "ReqIF.Prefix",
+            "ReqIF.Text"
+        ];
+
+    const tableHeaders = headers.map((header, count) => {
+        return <TableHeader name={header} key={`head${count}`}/>
+    });
+
+    function drop(e) {
+        for (const th of document.querySelectorAll(`th.${bgHover}`)) {
+            th.classList.remove(bgHover);
+        }
+    }
+
     return (
-        <thead>
+        <thead onDrop={drop}>
         <tr>
             {
-                headers.map((header, count) => {
-                        return <TableHeader name={header} key={`head${count}`}/>
-                    }
-                )
+                 tableHeaders.map(i => i)
             }
         </tr>
         </thead>
