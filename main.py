ad23adc48e742b261c65cc09e9dba7b2de91726e
MehdiBela <mbelatrous@gmail.com>
2021-01-22 13:31:24 +0100

    working on build

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from flask import Flask, render_template, Blueprint, request, Response
 
@@ -7,6 +8,11 @@ app = Flask(
     template_folder=os.path.join(os.path.dirname(__file__), "templates"),
     static_folder=os.path.join(os.path.dirname(__file__), "gui/build/static")
 )
+#  built settings
+if getattr(sys, 'frozen', False):
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, "static")
+    app = Flask(__name__, template_folder=template_folder, static_folder=static_folder)
 
 bp = Blueprint("index", __name__)
 
@@ -14,7 +20,7 @@ bp = Blueprint("index", __name__)
 @bp.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "GET":
-        return render_template("base.html")
+        return render_template("base.html", flask_token="HelloWorld")
 
 
 app.register_blueprint(bp)
