1c5fe44e4e5b0e3d2b0d5ef3018b665cac43d82a
MehdiBela <mbelatrous@gmail.com>
2021-02-01 19:35:31 +0100

    code cleanup

--- gui/src/components/table/Table.js
+++ gui/src/components/table/Table.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import TableHead from "./TableHead";
-import TableBody from "./TableBody";
 
 class Table extends React.Component {
     render() {
@@ -8,30 +7,7 @@ class Table extends React.Component {
             <div>
                 <table>
                     <TableHead/>
-                    <tbody>
-                    <tr>
-                        <td>contenu 1A</td>
-                        <td>contenu 2A</td>
-                        <td>contenu 3A</td>
-                        <td>contenu 4A</td>
-                        <td>contenu 5A</td>
-                        <td>contenu 6A</td>
-                        <td>contenu 7A</td>
-                        <td>contenu 8A</td>
-                        <td>contenu 9A</td>
-                    </tr>
-                    <tr>
-                        <td>contenu 1B</td>
-                        <td>contenu 2B</td>
-                        <td>contenu 3B</td>
-                        <td>contenu 4B</td>
-                        <td>contenu 5B</td>
-                        <td>contenu 6B</td>
-                        <td>contenu 7B</td>
-                        <td>contenu 8B</td>
-                        <td>contenu 9B</td>
-                    </tr>
-                    </tbody>
+                    <tbody/>
                 </table>
             </div>
         )
