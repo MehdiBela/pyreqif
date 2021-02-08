48b8448f8c7663253e2cf7903ecbd8550c8dec5c
MehdiBela <mbelatrous@gmail.com>
2021-02-08 19:57:54 +0100

    Header

--- gui/src/App.js
+++ gui/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import './style.css';
 import Table from "./components/table/Table";
 import Form from "./components/form/Form";
-import  "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 
 class App extends React.Component {
@@ -25,9 +25,13 @@ class App extends React.Component {
         return (
             <div className="App">
                 <div className={"grid"}>
+                    <div className={"row"}>
+                        <div className={"col-12 py-2 mb-2"} style={styles.bgFra}>
+                            <h2 className={"text-center"}>Excel to ReqIF Converter</h2>
+                        </div>
+                    </div>
                     <div className={"d-flex"}>
                         <div className={"mx-auto"}>
-                            <h1 className={"text-center py-2"}>Excel to ReqIF Converter</h1>
                             <Form onDataLoaded={this.dataLoaded}/>
                         </div>
                     </div>
@@ -40,7 +44,13 @@ class App extends React.Component {
             </div>
         );
     }
-
 }
+
+const styles = {
+    bgFra: {
+        backgroundColor: "#09357a",
+        color: "#ffffff"
+    }
+};
 
 export default App;
